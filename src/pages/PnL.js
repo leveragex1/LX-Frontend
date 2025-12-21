cdaef5fde59a955e0a2a6cb410bca0a466649ee1
leveragex1 <leveragex502@gmail.com>
2025-12-21 11:31:17 +0530

    Update PnL.js

--- src/pages/PnL.js
+++ src/pages/PnL.js
@@ -218,7 +218,7 @@ const PnL = () => {
               </div>
 
               <button
-                onClick={() => handleSell(stock.stockName, stock.quantity)}
+                onClick={() => handleSall(stock.stockName, stock.quantity)}
                 className="sell-btn"
               >
                 Sell
