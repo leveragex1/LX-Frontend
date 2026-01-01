b82bf958cfe700b7be36df6ee8e8beb62d116011
leveragex1 <leveragex502@gmail.com>
2026-01-01 14:43:21 +0530

    Update congrats.js

--- src/pages/congrats.js
+++ src/pages/congrats.js
@@ -49,7 +49,7 @@ const PayloadDialogForm = () => {
         onSubmit: async (values, { resetForm }) => {
             setSubmitted(true);
             try {
-                await axios.post('https://lx-backend-og1d.onrender.com/congrats', {
+                await axios.post('https://lx-backend-cn4r.onrender.com/congrats', {
                     Username: values.name,
                     Pancard: values.pancard,
                     UpiId: values.upiId,
