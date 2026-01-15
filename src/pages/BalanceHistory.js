8ccf10a93c129d09ab44d372cfd5d54ea5fe2386
leveragex1 <leveragex502@gmail.com>
2026-01-15 17:08:15 +0530

    Update BalanceHistory.js

--- src/pages/BalanceHistory.js
+++ src/pages/BalanceHistory.js
@@ -23,7 +23,7 @@ const WithdrawalHistory = () => {
     if (!email) return;
 
     axios
-      .get(`https://leveragexfund-4rz6.onrender.com/putBalance?email=${email}`)
+      .get(`https://lx-backend-0mh6.onrender.com/putBalance?email=${email}`)
       .then((res) => setHistory(res.data))
       .catch((err) => {
         console.error(err);
