b13adc174fa88aba74821d187e56bbefcc3dc2e9
leveragex1 <leveragex502@gmail.com>
2026-04-04 16:45:43 +0530

    Update WatchList2.js

--- src/pages/WatchList2.js
+++ src/pages/WatchList2.js
@@ -31,7 +31,7 @@ const WatchList1 = () => {
   const fetchStocks = async () => {
     try {
       const response = await axios.get(
-        "https://lx-backend-ta8m.onrender.com/api/watchlist2"
+        "https://lx-backend-uyu6.onrender.com/api/watchlist2"
       );
       setStocks(response.data);
     } catch (error) {
@@ -44,7 +44,7 @@ const WatchList1 = () => {
 
     try {
       const response = await axios.get(
-        `https://lx-backend-ta8m.onrender.com/api/users/balance/${userId}`
+        `https://lx-backend-uyu6.onrender.com/api/users/balance/${userId}`
       );
       setCurrentBalance(response.data.balance);
       setUpdatedBalance(response.data.balance);
@@ -89,7 +89,7 @@ const WatchList1 = () => {
 
     try {
       const response = await axios.post(
-        "https://lx-backend-ta8m.onrender.com/api/watchlist2/buy",
+        "https://lx-backend-uyu6.onrender.com/api/watchlist2/buy",
         {
           stockName: selectedOption.name,
           userId: userId,
