27838f0d76f388603360cdb72f2d6b12499ddfd1
leveragex1 <leveragex502@gmail.com>
2026-04-13 15:55:33 +0530

    Update Plans.js

--- src/pages/Plans.js
+++ src/pages/Plans.js
@@ -1,4 +1,4 @@
-```jsx
+
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
@@ -289,4 +289,3 @@ function Plans() {
 }
 
 export default Plans;
-```
