7d31e0b80f17d532b658dadab9d5b92065d595cf
leveragex1 <leveragex502@gmail.com>
2026-04-23 12:06:43 +0530

    Update Plans.js

--- src/pages/Plans.js
+++ src/pages/Plans.js
@@ -190,6 +190,7 @@ function Plans() {
                         )}
 
                         <p className='qr-p qr-pq'>leveragexfundon-3@oksbi</p>
+                        <p className='qr-p qr-pq'>leveragexfundon-1@okicici</p>
 
                         <img src={upiImg} alt="upi-logo" className='upi-img' />
 
