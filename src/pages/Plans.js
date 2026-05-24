ca4c0ce7896616de9a638876df79d2a6be06b28b
leveragex1 <leveragex502@gmail.com>
2026-05-24 12:15:29 +0530

    Update Plans.js

--- src/pages/Plans.js
+++ src/pages/Plans.js
@@ -173,7 +173,7 @@ function Plans() {
                         />
 
 
-                        <p className='qr-p qr-pq'>leveragexfundx-2@okicici</p>
+                        <p className='qr-p qr-pq'>7219088334@upi</p>
 
                         <p className='qr-p qr-pq'>leveragxcradit@ptyes</p>
 
