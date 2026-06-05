6b48f9a95d3c4e0a86b499079d92c40f5b33ef1b
leveragex1 <leveragex502@gmail.com>
2026-06-05 15:58:13 +0530

    Update Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -36,7 +36,7 @@ function Signup() {
         }
 
         try {
-            const url = `https://lx-backend-67az.onrender.com/auth/signup`;
+            const url = `https://backend-33cu.onrender.com/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
