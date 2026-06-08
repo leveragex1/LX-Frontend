cf4942df0552607b63f5772efe55f11e2d1a2ade
leveragex1 <leveragex502@gmail.com>
2026-06-08 13:27:26 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -29,7 +29,7 @@ function Login() {
             return handleError("Email and password are required");
         }
         try {
-            const url = `https://lx-backend-1-916j.onrender.com/auth/login`;
+            const url = `https://lx-backend-yj3l.onrender.com/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
